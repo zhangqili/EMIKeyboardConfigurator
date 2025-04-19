d8d8bd51937541fa9785a589c881dfa4e0b38e80
zhangqili <xq123666@outlook.com>
2025-04-19 15:54:41 +0800

    support zellia 80he

--- emi-keyboard-controller/src/controllers/zellia_80_controller/controller.ts
+++ emi-keyboard-controller/src/controllers/zellia_80_controller/controller.ts
@@ -1,14 +1,31 @@
+import { LibampKeyboardController } from '../libamp_keyboard_controller/controller';
 import { IAdvancedKey, IKeyboardController, IRGBConfig, KeyMode, CalibrationMode, RGBMode, Keycode, KeyModifier, AdvancedKeyToBytes, AdvancedKey, KeyboardController } from './../../interface';
 
 const layout = `[["Esc",{"x":0.25},"F1","F2","F3","F4",{"x":0.25},"F5","F6","F7","F8",{"x":0.25},"F9","F10","F11","F12",{"x":0.25},"Fn",{"x":0.25},"PrtSc","Scroll Lock","Pause\\nBreak"],[{"y":0.5},"~\\n\`","!\\n1","@\\n2","#\\n3","$\\n4","%\\n5","^\\n6","&\\n7","*\\n8","(\\n9",")\\n0","_\\n-","+\\n=",{"w":2},"Backspace",{"x":0.25},"Insert","Home","PgUp"],[{"w":1.5},"Tab","Q","W","E","R","T","Y","U","I","O","P","{\\n[","}\\n]",{"w":1.5},"|\\n\\\\",{"x":0.25},"Delete","End","PgDn"],[{"w":1.75},"Caps Lock","A","S","D","F","G","H","J","K","L",":\\n;","\\"\\n'",{"w":2.25},"Enter"],[{"w":2.25},"Shift","Z","X","C","V","B","N","M","<\\n,",">\\n.","?\\n/",{"w":2.75},"Shift",{"x":1.25},"↑"],[{"w":1.5},"Ctrl","Win",{"w":1.5},"Alt",{"a":7,"w":7},"",{"a":4,"w":1.5},"Alt","Win",{"w":1.5},"Ctrl",{"x":0.25},"←","↓","→"]]`;
 
-export class Zellia80Controller extends KeyboardController {
-    device: HIDDevice | undefined;
-    ADVANCED_KEY_NUM: number = 87;
+export class Zellia80Controller extends LibampKeyboardController {
+    ADVANCED_KEY_NUM: number = 88;
+    config_file_number:number = 4;
 
     constructor() {
         super();
         this.device = undefined;
+        this.reset_to_default();
+
+    }
+
+    async detect(): Promise<HIDDevice[]> {
+        return await navigator.hid.requestDevice({
+            filters: [{ vendorId: 0xFEED, productId: 22319, usagePage: 0xFF60}]  // 使用示例，过滤器可以根据需求进行调整
+        });;
+    }
+
+    get_layout_json(): string {
+        return layout;
+    }
+
+
+    reset_to_default(): void {
         this.advanced_keys = Array(this.ADVANCED_KEY_NUM).fill(null).map(() => ({
             state: false,
             value: 0.0,
@@ -44,9 +61,5 @@ export class Zellia80Controller extends KeyboardController {
                 Array(this.ADVANCED_KEY_NUM).fill(0)
         ];
 
-    }
-
-    get_layout_json(): string {
-        return layout;
     }
 }
