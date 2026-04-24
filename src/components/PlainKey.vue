697b58aba6f407cf1d5a7c54fcb257a815e52486
zhangqili <xq123666@outlook.com>
2026-04-24 23:48:31 +0800

    update key

--- src/components/PlainKey.vue
+++ src/components/PlainKey.vue
@@ -1,85 +1,50 @@
-<script setup>
-import { computed, reactive, ref } from "vue";
-import * as kle from "@ijprest/kle-serial";
-import { Translation } from "vue-i18n";
+<script setup lang="ts">
+import { computed, ref, type CSSProperties } from "vue";
+import { NButton } from "naive-ui";
 
 const props = defineProps([
-  "x",
-  "y",
-  "width",
-  "height",
-  "rotationX",
-  "rotationY",
-  "rotationAngle",
-  "labels",
-  "color",
-  "selected"
+  "x", "y", "width", "height", "rotationX", "rotationY", "rotationAngle",
+  "labels", "color", "selected", "tooltip"
 ]);
+
+const emit = defineEmits(['click']);
 
 const usize = ref(54);
 const margin = ref(2);
 
-const position = computed(() => {
-  return {
-    position: "absolute",
-    left: usize.value * props.x + "px",
-    top: usize.value * props.y + "px",
-  };
-});
-
-const size = computed(() => {
-  return {
-    width: usize.value * props.width + "px",
-    height: usize.value * props.height + "px",
-  };
-});
-
-const keycap_size = computed(() => {
-  return {
-    width: usize.value * props.width + "px",
-    height: usize.value * props.height + "px",
-    position: "absolute",
-    left: usize.value * props.x + "px",
-    top: usize.value * props.y + "px",
-    transition: 'all 0.5s ease'
-  };
-});
-
-const sizeKeytop = computed(() => {
-  return {
-    width: usize.value * props.width - 12 + "px",
-    height: usize.value * props.height - 12 + "px",
-  };
-});
-
-const sizeLabel = computed(() => {
-  return {
-    width: usize.value * props.width - 12 + "px",
-    height: usize.value * props.height - 12 + "px",
-    maxWidth: usize.value * props.width - 12 + "px",
-    maxHeight: usize.value * props.height - 12 + "px",
-    textWrap: "wrap",
-  };
-});
-
-const sizeLabel1 = computed(() => {
-  return {
-    width: usize.value * props.width - 12 + "px",
-    maxWidth: usize.value * props.width - 12 + "px",
-    textWrap: "wrap",
-    height: "8px",
-  };
-});
-const rotation = computed(() => {
+const rotation = computed(() : CSSProperties => {
   if (props.rotationAngle !== 0) {
     return {
       transform: `rotate(${props.rotationAngle}deg)`,
-      transformOrigin: `${usize.value * props.rotationX}px ${usize.value * props.rotationY
-        }px`,
+      transformOrigin: `${usize.value * props.rotationX}px ${usize.value * props.rotationY}px`,
     };
   }
   return {};
 });
+
+const keycap_size = computed(() : CSSProperties => ({
+  width: usize.value * props.width + "px",
+  height: usize.value * props.height + "px",
+  position: "absolute",
+  left: usize.value * props.x + "px",
+  top: usize.value * props.y + "px",
+  transition: 'all 0.5s ease'
+}));
+
+const sizeLabel = computed(() : CSSProperties => ({
+  width: usize.value * props.width - 12 + "px",
+  height: usize.value * props.height - 12 + "px",
+  maxWidth: usize.value * props.width - 12 + "px",
+  maxHeight: usize.value * props.height - 12 + "px",
+  textWrap: "wrap",
+}));
+
+const sizeLabel1 = computed(() : CSSProperties => ({
+  width: usize.value * props.width - 12 + "px",
+  maxWidth: usize.value * props.width - 12 + "px",
+  textWrap: "wrap",
+  height: "8px",
+}));
 
 const key_border = computed(() => {
   return {
@@ -87,7 +52,7 @@ const key_border = computed(() => {
   };
 });
 
-function getContrastColor(bgColor) {
+function getContrastColor(bgColor : string) {
     if (bgColor == undefined) {
       return;
     }
@@ -98,14 +63,21 @@ function getContrastColor(bgColor) {
     return brightness > 128 ? "#000000" : "#FFFFFF";
 }
 
-const button_style = computed(() => {
-  return {
+const button_style = computed(() : any => {
+  const styleObj: any = {
     height: "100%",
     width: "100%",
-    background: props.color,
-    color: getContrastColor(props.color),
-    outline: props.selected ? "solid red 2px" : "",
+    outline: props.selected ? "solid red 2px" : "none",
   };
+
+if (props.color) {
+    styleObj["--n-border"] = `1px solid ${props.color}`;
+    styleObj["--n-border-hover"] = `2px solid ${props.color}`;
+    styleObj["--n-color-hover"] = `${props.color}40`; 
+    styleObj["--n-text-color-hover"] = getContrastColor(props.color) || "#FFFFFF";
+  }
+
+  return styleObj;
 });
 </script>
 
@@ -113,14 +85,27 @@ const button_style = computed(() => {
   <div class="key" :style="rotation">
     <div :style="keycap_size">
       <div style="position: absolute; inset: 2px;">
-        <n-button :style="button_style" :focusable="false" class="keycap" >
-          <div class="keylabels">
-            <div v-for="(label, index) in props.labels" :key="index" :class="'keylabel keylabel' + index + ' textsize2'">
-              <div v-if="index<9" :style="sizeLabel">{{ label }}</div>
-              <div v-if="index>=9" :style="sizeLabel1">{{ label }}</div>
-            </div>
+        
+        <n-popover 
+          :disabled="!props.tooltip || props.tooltip.length === 0" 
+          trigger="hover" placement="top" :animated="false" :delay="0" :duration="0"
+        >
+          <template #trigger>
+            <n-button :style="button_style" :focusable="false" class="keycap" @click="$emit('click')">
+              <div class="keylabels">
+                <div v-for="(label, index) in props.labels" :key="index" :class="'keylabel keylabel' + index + ' textsize2'">
+                  <div v-if="index as number <9 && label" :style="sizeLabel">{{ label }}</div>
+                  <div v-if="index as number >=9 && label" :style="sizeLabel1">{{ label }}</div>
+                </div>
+              </div>
+            </n-button>
+          </template>
+          
+          <div style="text-align: center; font-size: 12px;">
+            <div v-for="(line, idx) in props.tooltip" :key="idx">{{ line }}</div>
           </div>
-        </n-button>
+        </n-popover>
+        
       </div>
     </div>
   </div>
@@ -258,11 +243,12 @@ const button_style = computed(() => {
   line-height: 1em;
 }
 
-/*
 .keylabels {
+/*
   font-family: "Helvetica", "Arial", sans-serif;
-}
 */
+  z-index: 1;
+}
 
 .keylabel hr {
   display: inline;
